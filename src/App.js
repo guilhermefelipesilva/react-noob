2379dc777cd2711c5c438ce55f02d2ac9df96745
guilhermereis-luizalabs <guilherme.reis@luizalabs.com>
2018-04-17 00:59:45 -0300

    state, function onClick

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Guilherme', age: 26 },
+      { name: 'Karen', age: 23 },
+      { name: 'Isadora', age: 21 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Cliquei')
+    this.setState({
+      persons: [
+        { name: 'Guilherme Felipe', age: 26 },
+        { name: 'Karen', age: 23 },
+        { name: 'Isadora', age: 23 }
+      ]
+    }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Olá Guilherme!</h1>
-        <Person name="Guilherme" age="26" />
-        <Person name="Paola" age="29" >My Hobbie is sleep</Person>
-        <Person name="Karen" age="23" />
+        <button onClick={this.switchNameHandler}>Mudar de nome</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbie is sleep</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
